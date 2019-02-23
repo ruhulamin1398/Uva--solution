85ba4e8f3619eda2ffab688a7d16b4ee500cd41f
ruhulaminrul <ruhul.ok@gmail.com>
2019-02-23 09:49:49 +0600

    1D Array

---
+++ 12356 - Army Buddies.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int l[100010], r[100010];
+int main()
+{
+    int tc,kk=1, n, q, left, right;
+    while(cin>>n>>q)
+    {
+        if(!n && !q) return 0;
+        for(int i=1;i<=n;i++)
+            l[i]=i-1, r[i]=i+1;
+        r[n]=0;
+        while(q--)
+        {
+            cin>>left>>right;
+            l[r[right]] = l[left];
+            r[l[left]] = r[right];
+            if(l[left]) cout<<l[left]<<" ";
+            else cout<<"* ";
+            if(r[right]) cout<<r[right]<<endl;
+            else cout<<"*"<<endl;
+        }
+        cout<<"-"<<endl;
+    }
+    return 0;
+}
